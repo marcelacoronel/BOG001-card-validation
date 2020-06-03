eaa5622b0ee8d5d7f9e80a5a9388e6828e42bb60
Alefyyyy <Alejndraprz@gmail.com>
2020-06-03 11:23:09 -0500

    Onclick event para obtener numero

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import validator from './validator.js';
 
-console.log(validator);
+function getInputCardValue(){
+    
+    const creditCardNumber = document.getElementById("creditCardNumber").value;
+    const creditCardNumberString = creditCardNumber.toString(); //check de que es una string 
+    const creditCardNumberArray = creditCardNumberString.split(); //check que es un array
+
+    console.log(creditCardNumber);
+
+    console.log(creditCardNumberString);
+
+    console.log(creditCardNumberArray[0]);
+    console.log(creditCardNumberArray[1]);
+    console.log(creditCardNumberArray[2]);
+    console.log(creditCardNumberArray[3]);
+
+    return creditCardNumberArray;
+}
+
+
+
+
+//console.log(validator);

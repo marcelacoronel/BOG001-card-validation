c3aa50d79b4a8f708fc87bb0062ecd2cfc1f560d
marcelacoronel <marcelacoronelp@gmail.com>
2020-06-12 11:39:48 -0500

    Cambios Marcela index.html

--- src/index.js
+++ src/index.js
@@ -3,19 +3,18 @@ import validator from './validator.js';
 document.getElementById('botonEnviar').addEventListener('click',  getInputCardValue);
 
 function getInputCardValue() {
-  // document.getElementById('cuadros').style.display('none');
-  // document.getElementById('vista2').style.display('block');
+ 
   let creditCardNumber = document.getElementById('cardNumber').value;
-
   let respuestaIsvalid= validator.isValid(creditCardNumber);
-    if (respuestaIsvalid==true){
-      document.getElementById('rtaIsvalid').value=respuestaIsvalid;
-       }
-      else{
-       document.getElementById('rtaIsvalid').value=respuestaIsvalid;
-         } 
+  document.getElementById('rtaIsvalid').innerHTML=respuestaIsvalid;
   let enmascarar=validator.maskify(creditCardNumber);
-    document.getElementById('respuestaMaskify').innerHTML=enmascarar;
-    // ("Su número de tarjeta: ")Mensaje numero enmascarado
-       
-   }
+  document.getElementById('respuestaMaskify').innerHTML=enmascarar;
+  document.getElementById('cuadros').style.display('none');
+  if (respuestaIsvalid==true){
+     document.getElementById('vista2').style.display('block');  
+      }
+  else{
+  document.getElementById('vista3').style.display('block');  
+  }
+      
+}
